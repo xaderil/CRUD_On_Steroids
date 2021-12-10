fa6e46373c4db9b7fd36e2314ac7f3d98ae6c64f
Xaderil <hjbgbj@gmail.com>
2021-12-10 18:14:08 +1000

    added Book create form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Form\BookType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,45 +13,62 @@ use Throwable;
 
 class HomeController extends AbstractController
 {
+
     /**
      * @Route("/", name="home")
      */
     public function index(Request $request): Response
     {
-
-        $authors = ['Dicker Dickers', 'Dostoevsky'];
-        $em = $this->getDoctrine()->getManager();
-
         $book = new Book();
-        $book->setTitle('War and peace');
-        $book->setDescription('Voevali');
-        $book->setPublicationYear(2020);
-        $book->setAuthorsCount(count($authors));
-        $em->persist($book);
-        $em->flush();
-
-        foreach ($authors as $authorName) {
-            if ($em->getRepository(Author::class)->findOneBy(array('name' => $authorName))) {
-                $author = $em->getRepository(Author::class)->findOneBy(array('name' => $authorName));
-                $author->setBooksCount($author->getBooksCount() + 1);
-                $author->addBook($book);
-            } else {
-                $author = new Author();
-                $author->setName($authorName);
-                $author->addBook($book);
-                $author->setBooksCount(1);
-                $em->persist($author);
-            }
-            $em->flush();
-
-
-
-
+        for ($i = 0; $i < HomeController::$authorsCount; $i++) {
+            $book->addAuthor(new Author());
         }
 
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //        $authors = ['Dicker Dickers', 'Dostoevsky'];
+            //        $em = $this->getDoctrine()->getManager();
+            //
+            //        $book = new Book();
+            //        $book->setTitle('War and peace');
+            //        $book->setDescription('Voevali');
+            //        $book->setPublicationYear(2020);
+            //        $book->setAuthorsCount(count($authors));
+            //        $em->persist($book);
+            //        $em->flush();
+            //
+            //        foreach ($authors as $authorName) {
+            //            if ($em->getRepository(Author::class)->findOneBy(array('name' => $authorName))) {
+            //                $author = $em->getRepository(Author::class)->findOneBy(array('name' => $authorName));
+            //                $author->setBooksCount($author->getBooksCount() + 1);
+            //                $author->addBook($book);
+            //            } else {
+            //                $author = new Author();
+            //                $author->setName($authorName);
+            //                $author->addBook($book);
+            //                $author->setBooksCount(1);
+            //                $em->persist($author);
+            //            }
+            //            $em->flush();
+            //
+            //        }
+        }
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form' => $form->createView(),
+            'auths' => HomeController::$authorsCount
         ]);
+    }
+
+    /**
+     * @Route("/inc", name="checkAuthorsCount")
+     */
+    public function incrementAuthorsCount(): \Symfony\Component\HttpFoundation\RedirectResponse
+    {
+        HomeController::$authorsCount++;
+        return $this->redirectToRoute('home');
     }
 }
---
+++ src/Form/BookType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+            ->add('publicationYear', NumberType::class)
+            ->add('authors',CollectionType::class, array(
+                'entry_type' => AuthorType::class,
+                'entry_options' => ['label' => false]))
+            ->add('send', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
