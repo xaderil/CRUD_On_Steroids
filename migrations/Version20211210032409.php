4b1f74b6207b1ce6929db2d65b93aa64498b6edf
Xaderil <hjbgbj@gmail.com>
2021-12-10 15:41:45 +1000

    created create query sketch

--- migrations/Version20211210032409.php
+++ migrations/Version20211210032409.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211209143403 extends AbstractMigration
+final class Version20211210032409 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20211209143403 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD name VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BDAFD8C85E237E06 ON author (name)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP name');
+        $this->addSql('DROP INDEX UNIQ_BDAFD8C85E237E06 ON author');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Throwable;
 
 class HomeController extends AbstractController
 {
@@ -16,10 +17,37 @@ class HomeController extends AbstractController
      */
     public function index(Request $request): Response
     {
+
+        $authors = ['Dicker Dickers', 'Dostoevsky'];
         $em = $this->getDoctrine()->getManager();
 
-        $author = new Author();
         $book = new Book();
+        $book->setTitle('War and peace');
+        $book->setDescription('Voevali');
+        $book->setPublicationYear(2020);
+        $book->setAuthorsCount(count($authors));
+        $em->persist($book);
+        $em->flush();
+
+        foreach ($authors as $authorName) {
+            if ($em->getRepository(Author::class)->findOneBy(array('name' => $authorName))) {
+                $author = $em->getRepository(Author::class)->findOneBy(array('name' => $authorName));
+                $author->setBooksCount($author->getBooksCount() + 1);
+                $author->addBook($book);
+            } else {
+                $author = new Author();
+                $author->setName($authorName);
+                $author->addBook($book);
+                $author->setBooksCount(1);
+                $em->persist($author);
+            }
+            $em->flush();
+
+
+
+
+        }
+
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
