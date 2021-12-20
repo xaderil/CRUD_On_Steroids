296c95ac0cf6494cd8cce07b958212fe1b94b606
Xaderil <hjbgbj@gmail.com>
2021-12-21 00:17:03 +1000

    fixed bug with non-working add author button in create book form

--- public/assets/js/addAuthorField.js
+++ public/assets/js/addAuthorField.js
@@ -1,6 +1,6 @@
 
 var template = '<div class="input-group help-block"><input type="text" name = "authors[]" class="form-control" placeholder="Автор"/></div>';
-var minusButton = '<div class="input-group-append delete-field"><span class="btn input-group-text delete-field"><i class="fa fa-minus-square"></i></span></div>';
+var minusButton = '<div class="input-group-append delete-fieldEdit"><span class="btn input-group-text delete-field"><i class="fa fa-minus-square"></i></span></div>';
 
 $('.add-field').click(function (node, child) {
     var temp = $(template);
@@ -8,6 +8,6 @@ $('.add-field').click(function (node, child) {
     $('.help-block').last().append(temp);
 });
 
-$(document).on('click', '.delete-field', function(){
+$(document).on('click', '.delete-fieldEdit', function(){
     $(this).parents('.input-group').first().remove();
 });
--- public/assets/js/addAuthorFieldForEdit.js
+++ public/assets/js/addAuthorFieldForEdit.js
@@ -1,11 +1,11 @@
 
-var template = '<div class="input-group help-block"><input type="text" name = "authors[]" class="form-control" placeholder="Автор"/></div>';
+var template = '<div class="input-group help-blockEdit"><input type="text" name = "authors[]" class="form-control" placeholder="Автор"/></div>';
 var minusButton = '<div class="input-group-append delete-field"><span class="btn input-group-text delete-field"><i class="fa fa-minus-square"></i></span></div>';
 
 $('.add-fieldEdit').click(function (node, child) {
     var temp = $(template);
     temp.append(minusButton);
-    $('.help-block').last().append(temp);
+    $('.help-blockEdit').last().append(temp);
 });
 
 $(document).on('click', '.delete-field', function(){
